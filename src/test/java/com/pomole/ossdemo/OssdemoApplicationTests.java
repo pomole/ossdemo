045f490f6d4b1e5b639aa150c7304cef97b4bfed
pomole <203044987@qq.com>
2019-10-26 07:17:08 +0800

    Initial commit

---
+++ src/test/java/com/pomole/ossdemo/OssdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pomole.ossdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OssdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
